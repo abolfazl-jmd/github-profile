a5536f37366348c5c0849081d84337916a305409
Abolfazl Jamshidi <jmd.abolfazl14@yahoo.com>
2022-08-11 10:34:14 +0430

    load user's profile details feature

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -1,10 +1,29 @@
 import SearchInput from "../Components/InputComponent/InputComponent";
 import styles from "./HomePage.module.css";
+import { useNavigate } from "react-router-dom";
+import { useState } from "react";
 
 const HomePage = () => {
+  const [searchValue, setSearchValue] = useState("");
+  const navigate = useNavigate();
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    console.log(searchValue);
+
+    navigate(`/username?=${searchValue}`);
+
+    // cleaning the input
+    setSearchValue("");
+  };
+
   return (
     <div className={styles.wrapper}>
-      <SearchInput />
+      <SearchInput
+        submitHandler={submitHandler}
+        searchValue={searchValue}
+        setSearchValue={setSearchValue}
+      />
     </div>
   );
 };
